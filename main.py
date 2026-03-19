24cd446afe7f205af38aa3c24c04c8d38b1885c3
Juanpoveda115 <juandavidpoa@gmail.com>
2026-03-19 08:25:11 -0500

    Task 3: Show all the products in inventory (For loop)

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-inventory = []
+inventory = [] 
 product = {}
 option = None
 while option != "4":
-    option = input("1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
+    option = input("\n1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
 
     if option == "1":
         print("\nYou chose to add a product (option 1)\n")
         productName = input("Enter the product's name:  ")
         productPrice = float(input(f"Enter the {productName} price: "))
         productQuantity = int(input(f"Enter the {productName} quantity: "))
-        
+        print("\nProduct sucessfuly added!")
+
         product = {
             "name": productName,
             "price": productPrice,
@@ -19,7 +20,14 @@ while option != "4":
         inventory.append(product)
     
     elif option == "2":
-        print(f"show\n\n{inventory}\n\n{product}\n") #testing
+        print("\nYou chose to show the inventory (option 2)")
+        if not inventory:
+            print("\nThe inventory is empty")
+        else:
+            for idx, productInv in enumerate(inventory):
+                print(f"\nProduct {idx+1}:")
+                print(f"Name: {productInv["name"]} | Price: {productInv["price"]} | Quantity: {productInv["quantity"]}")
+
     elif option == "3":
         print("calculate") #testing
     elif option == "4":
