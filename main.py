b52c22defd5b3634c0687bab96cadcb639d4770b
Juanpoveda115 <juandavidpoa@gmail.com>
2026-03-19 08:36:05 -0500

    Task 4: Calculate basic statistics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 inventory = [] 
 product = {}
 option = None
+totalProduct = 0
+totalInventory = 0
 while option != "4":
     option = input("\n1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
 
@@ -29,8 +31,15 @@ while option != "4":
                 print(f"Name: {productInv["name"]} | Price: {productInv["price"]} | Quantity: {productInv["quantity"]}")
 
     elif option == "3":
-        print("calculate") #testing
+        print("\nYou chose to calculate the inventory statistics (option 3)") 
+        if not inventory:
+            print("\nThe inventory is empty, there's nothing to calculate")
+        else:
+            for productInv in inventory:
+                totalProduct = productInv["price"] * productInv["quantity"]
+                totalInventory += totalProduct
+            print(f"\nTotal value in inventory: {totalInventory}\nTotal Products Registered: {len(inventory)}")
     elif option == "4":
-        print("leave") #testing
+        print("Goodbye!!")
     else:
         print("Invalid input, try again with one of the options (1-4)")
