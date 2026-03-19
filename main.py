8b0ea94eca0527be047e36041f18e5bf0b91586d
Juanpoveda115 <juandavidpoa@gmail.com>
2026-03-19 06:44:44 -0500

    Task 1: Data validation with conditionals (Menu)

---
+++ main.py
@@ -0,0 +1,12 @@
+opcion = input("1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
+
+if opcion == "1":
+
+elif opcion == "2":
+
+elif opcion == "3":
+
+elif opcion == "4":
+
+else:
+    print("Invalid input, try again with one of the options (1-4)")
