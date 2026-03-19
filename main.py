737952f750b7d14bca1b22c2a1bce58fc774a427
Juanpoveda115 <juandavidpoa@gmail.com>
2026-03-19 07:08:01 -0500

    Task 2: Implementation of a loop for multiple records (While loop, dictionary and list)

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-opcion = input("1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
+inventory = []
+product = {}
+option = None
+while option != "4":
+    option = input("1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
 
-if opcion == "1":
+    if option == "1":
+        print("\nYou chose to add a product (option 1)\n")
+        productName = input("Enter the product's name:  ")
+        productPrice = float(input(f"Enter the {productName} price: "))
+        productQuantity = int(input(f"Enter the {productName} quantity: "))
+        
+        product = {
+            "name": productName,
+            "price": productPrice,
+            "quantity": productQuantity
+        }
 
-elif opcion == "2":
-
-elif opcion == "3":
-
-elif opcion == "4":
-
-else:
-    print("Invalid input, try again with one of the options (1-4)")
+        inventory.append(product)
+    
+    elif option == "2":
+        print(f"show\n\n{inventory}\n\n{product}\n") #testing
+    elif option == "3":
+        print("calculate") #testing
+    elif option == "4":
+        print("leave") #testing
+    else:
+        print("Invalid input, try again with one of the options (1-4)")
